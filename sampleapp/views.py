c34e0d37aa4a1c67c1ea46353cf76c67ae483662
Deshraj <deshrajdry@gmail.com>
2015-09-26 03:59:22 +0530

    sampleapp modified with adding of base template

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+	'''
+	Home page view for socket connections 
+	'''
+	return render_to_response('index.html', )
+
